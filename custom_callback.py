69e1596c1a7960d6d37e8e937376b694b2a09cd0
avocardio <mkalcher01@gmail.com>
2022-12-04 16:46:17 +0100

    New working custom callback (with timestep)

---
+++ custom_callback.py
@@ -0,0 +1,97 @@
+
+import tensorflow as tf
+import csv
+import os
+from keras.utils import io_utils
+import collections
+import numpy as np
+import datetime
+
+class CSVLogger(tf.keras.callbacks.Callback):
+    """Callback that streams epoch results + timestamps to a CSV file.
+
+    Supports all values that can be represented as a string,
+    including 1D iterables such as `np.ndarray`.
+
+    Example:
+
+    ```python
+    csv_logger = CSVLogger('training.log')
+    model.fit(X_train, Y_train, callbacks=[csv_logger])
+    ```
+    Args:
+        filename: Filename of the CSV file, e.g. `'run/log.csv'`.
+        separator: String used to separate elements in the CSV file.
+        append: Boolean. True: append if file exists (useful for continuing
+            training). False: overwrite existing file.
+    """
+
+    def __init__(self, filename, separator=",", append=False):
+        self.sep = separator
+        self.filename = io_utils.path_to_string(filename)
+        self.append = append
+        self.writer = None
+        self.keys = None
+        self.append_header = True
+        super().__init__()
+
+    def on_train_begin(self, logs=None):
+        if self.append:
+            if tf.io.gfile.exists(self.filename):
+                with tf.io.gfile.GFile(self.filename, "r") as f:
+                    self.append_header = not bool(len(f.readline()))
+            mode = "a"
+        else:
+            mode = "w"
+        self.csv_file = tf.io.gfile.GFile(self.filename, mode)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, str):
+                return k
+            elif (
+                isinstance(k, collections.abc.Iterable)
+                and not is_zero_dim_ndarray
+            ):
+                return f"\"[{', '.join(map(str, k))}]\""
+            else:
+                return k
+
+        if self.keys is None:
+            self.keys = sorted(logs.keys())
+
+        if self.model.stop_training:
+            # We set NA so that csv parsers do not fail for this last epoch.
+            logs = dict(
+                (k, logs[k]) if k in logs else (k, "NA") for k in self.keys
+            )
+
+        if not self.writer:
+
+            class CustomDialect(csv.excel):
+                delimiter = self.sep
+
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            fieldnames = ["epoch", "timestamp"] + self.keys
+
+            self.writer = csv.DictWriter(
+                self.csv_file, fieldnames=fieldnames, dialect=CustomDialect
+            )
+            if self.append_header:
+                self.writer.writeheader()
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        row_dict = collections.OrderedDict(
+            {"epoch": epoch, "timestamp": timestamp}
+        )
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+        self.writer.writerow(row_dict)
+        self.csv_file.flush()
+
+    def on_train_end(self, logs=None):
+        self.csv_file.close()
+        self.writer = None
