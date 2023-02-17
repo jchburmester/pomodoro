0991e3ac862147fb3ae205c441d105e7c8dd2834
avocardio <mkalcher@uni-osnabrueck.de>
2023-02-16 23:40:55 -0600

    refactored subfolder creation

--- utils/subfolder_creation.py
+++ utils/subfolder_creation.py
@@ -4,7 +4,7 @@ def create_subfolder():
     """
     Makes sure that headfolder 'runs' exists to store information about current run.
     Checks which runs subfolders already exist and creates subsequent one. 
-    Subfolders are named by the index of their run.
+    Subfolders are named by the index of their run, starting with 000.
     """
     # get parent directory
     parent_dir = os.getcwd()
@@ -12,27 +12,26 @@ def create_subfolder():
     # create head folder in parent directory to store the subfolders
     head_folder_path = os.path.join(parent_dir, 'runs')
 
-    if not os.path.exists(head_folder_path):
-        os.makedirs(head_folder_path)
+    os.makedirs(head_folder_path, exist_ok=True)
 
     # stores all existing runs subfolders
     runs_dirs = sorted(os.listdir(head_folder_path))
 
-    # if first run, creates subfolder with index 0
+    # if first run, creates subfolder with index 000
     if len(runs_dirs) == 0:
-        os.makedirs(os.path.join(head_folder_path, str(0)))
-        current_dir = str(0)
+        os.makedirs(os.path.join(head_folder_path, '001'))
+        current_dir = '001'
     
-    # make sure that numbers lower 10 get preceded by '0', for sorting reasons
-    elif len(runs_dirs) < 10:
-        last_index = runs_dirs[-1]
-        os.makedirs(os.path.join(head_folder_path, '0' + str(int(last_index)+1)))
-        current_dir = '0' + str(int(last_index)+1)
-
-    # if subsequent run, creates subfolder with next index
+    # make sure that subfolder names start with '0' and have three digits
     else:
         last_index = runs_dirs[-1]
-        os.makedirs(os.path.join(head_folder_path, str(int(last_index)+1)))
-        current_dir = str(int(last_index)+1)
+        last_index_num = int(last_index)
+        next_index_num = last_index_num + 1
+        next_index_str = str(next_index_num).zfill(3)
+        os.makedirs(os.path.join(head_folder_path, next_index_str))
+        current_dir = next_index_str
+
+    def test():
+        return 0
     
-    return current_dir
+    return os.path.join(head_folder_path, current_dir)
