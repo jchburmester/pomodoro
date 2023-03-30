921365752c8dd287b9355af7cf8e4ac195460638
avocardio <mkalcher@uni-osnabrueck.de>
2023-03-30 13:43:49 +0200

    Removed old resnet50 file

--- models/resnet50_prebuilt.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-# Import the ResNet50 model
-def load_resnet50(classes, input_shape, weights=None):
-    model = tf.keras.applications.resnet50.ResNet50(classes=classes, input_shape=input_shape, weights=weights)
-    return model
